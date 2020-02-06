41bde5f833f5619b5f65fa933865e672cdfe61d5
Seniru <senirupasan@gmail.com>
2020-02-06 15:25:13 +0530

    Improved the digits predictor model
    - Switched from SGDClassifier to RandomForestClassifier

--- Digits/index.py
+++ Digits/index.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
 
-from sklearn.linear_model import SGDClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 digits = load_digits()
 
@@ -13,15 +13,7 @@ X, y = digits.data, digits.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
-y_train_5 = (y_train == 5)
-y_test_5 = (y_test == 5)
+rforest = RandomForestClassifier(random_state=42)
+rforest.fit(X_train, y_train)
 
-sgd_clf = SGDClassifier(random_state=42)
-
-sgd_clf.fit(X_train, y_train_5)
-
-print(sgd_clf.predict([X_train[0]]))
-
-for data, target in zip(X_train[:15], y_train[:15]):
-	print("Predicted: {}, Actual: {} ({})".format(sgd_clf.predict([data])[0], target == 5, target))
-
+print(rforest.predict(digits.data[:10]))
